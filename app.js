0882d68ba182def4acb11fc50ec92275db0f1dc8
Richard Clark <dcla839@aucklanduni.ac.nz>
2022-07-28 15:34:12 +1200

    s4

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-
-
-
-
 const express = require('express');
 const app = express();
+const config = require('config');
+
+// config
+
+// debug
 const serverDebug = require('debug')('Server:debug-Msg');
 const databaseDebug = require('debug')('Database:debug-Msg');
 const appDebug = require('debug')('app:debug-Msg');
-const config = require('config');
 
 // view-engine
 app.use('view-engine', ejs);
@@ -16,8 +16,8 @@ app.use('view-engine', ejs);
 app.use(express.json());
 app.use(express.urlencoded)
 
+//database-connection
 
-//- database-connection
 
 
 
